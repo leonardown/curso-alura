b2889e3b233f36e93ec011a243dc941d224ca05b
leonardown <75744039+leonardown@users.noreply.github.com>
2020-12-16 22:10:25 -0300

    Add files via upload

---
+++ TestaPontoFlutuantes.java
@@ -0,0 +1,19 @@
+
+public class TestaPontoFlutuantes {
+
+	public static void main(String[] args) {
+		double salario;
+		salario=1250.70;
+		System.out.println("Meu salário é " + salario);
+		
+		double divisao =3.14/2;
+		System.out.println(divisao);
+		
+		double preco = 5.5;
+		int ingressos = 4;
+		System.out.println(preco * ingressos);
+		
+		
+		
+	}
+}
